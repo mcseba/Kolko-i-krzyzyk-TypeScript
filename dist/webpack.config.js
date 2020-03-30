e7455ea4adbc500b0255751e9d18b9ea82cf6f27
mcseba <48526736+mcseba@users.noreply.github.com>
2020-03-30 17:30:10 +0200

    podstawa

---
+++ dist/webpack.config.js
@@ -0,0 +1,20 @@
+var path = require('path');
+module.exports = {
+    entry: './index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'tsc',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
---
+++ index.ts
@@ -0,0 +1,11 @@
+class Board {
+    table: Array<HTMLButtonElement>
+
+    constructor (Table: Array<HTMLButtonElement>) {
+        this.table = Table;
+    }
+}
+
+class Cell {
+
+}
